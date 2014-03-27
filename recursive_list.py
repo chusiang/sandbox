09a3d66426e7720ae5658497f574eea81c85bce2
chusiang <chusiang.lai@gmail.com>
2014-03-28 01:27:07 +0800

    《深入淺出 Python》第一章的 list, recursive lab。

---
+++ recursive_list.py
@@ -0,0 +1,19 @@
+# 初始變數
+movies = [
+	'a', 1991, 'c', 2001, 'd', 2010, 'e', 2014, 'g', 2020,
+			['h', 11, 'i', 12,
+					['j', 21, 'k', 22]]]
+# 宣告遞迴函數
+def print_lol(the_list):
+	for each_item in the_list:
+		if (isinstance(each_item, list)):
+			print_lol(each_item)
+		else:
+			print(each_item)
+
+# 印出初始變數
+print('movies = ' + str(movies) + '\n')
+
+# 印出結果
+print('result: \n')
+print_lol(movies)
