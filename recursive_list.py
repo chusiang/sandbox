d8c8e0c87db8794e4ffebb500bee8c124fd7d830
chusiang <chusiang.lai@gmail.com>
2014-04-25 14:12:10 +0800

    調整縮排。

--- recursive_list.py
+++ recursive_list.py
@@ -1,15 +1,16 @@
 # 初始變數
 movies = [
-	'a', 1991, 'c', 2001, 'd', 2010, 'e', 2014, 'g', 2020,
-			['h', 11, 'i', 12,
-					['j', 21, 'k', 22]]]
-# 宣告遞迴函數
+    'a', 1991, 'c', 2001, 'd', 2010, 'e', 2014, 'g', 2020,
+    ['h', 11, 'i', 12,
+      ['j', 21, 'k', 22]]]
+
+    # 宣告遞迴函數
 def print_lol(the_list):
-	for each_item in the_list:
-		if (isinstance(each_item, list)):
-			print_lol(each_item)
-		else:
-			print(each_item)
+  for each_item in the_list:
+    if (isinstance(each_item, list)):
+      print_lol(each_item)
+    else:
+      print(each_item)
 
 # 印出初始變數
 print('movies = ' + str(movies) + '\n')
