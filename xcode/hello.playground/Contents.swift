a1337a132ecf4b8e1e85f35eef57638a9d05bcd5
chusiang <chusiang.lai@gmail.com>
2015-04-09 22:51:56 +0800

    add hello.playground.

---
+++ xcode/hello.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+/*
+ * print 10 time.
+ */
+for _ in 1 ... 10 {
+  println("Hello, playground")
+}
+
+/*
+ * print 10 time, and string append.
+ */
+for var i = 1; i <= 10; i++ {
+  println("Hello, playground x \(i)")
+}
