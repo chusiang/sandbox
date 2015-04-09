4aca089c86005051253f3b90b644a55995971be9
chusiang <chusiang.lai@gmail.com>
2015-04-09 20:48:02 +0800

    add hello world with swift.

---
+++ swift/hello.swift
@@ -0,0 +1,22 @@
+#!/usr/bin/swift
+
+
+println("Hello Swift!")
+
+
+/*
+ * print 10 time.
+ */
+println("---- print 10 time ----")
+for _ in 1...10 {
+  println("Hello Swift! x 10")
+}
+
+/*
+ * print 10 time, and string append.
+ */
+println("---- print 10 time, and string append ----")
+for var i = 1; i <= 10; i++ {
+  println("Hello Swift! x \(i)")
+}
+
