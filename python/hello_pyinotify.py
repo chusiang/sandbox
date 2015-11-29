fd82bd9bea32b9918f76f2165893351c075a206b
chusiang <chusiang.lai@gmail.com>
2015-11-30 00:45:13 +0800

    fix indent

--- python/hello_pyinotify.py
+++ python/hello_pyinotify.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+# ============================================================
+#  Author: 凍仁翔 / chusiang.lai (at) gmail.com
+#  Blog: http://note.drx.tw
+#  Filename: hello_pyinotify.py
+#  Modified: 2015-11-29 23:55
+#  Description: monitors transient files. 
+#  Reference: 
+#  
+#   - monitors transient files.
+#    - https://github.com/seb-m/pyinotify/blob/master/python2/examples/transient_file.py
+#
+# =========================================================== 
 
-# Example: monitors transient files.
-#
-# - https://github.com/seb-m/pyinotify/blob/master/python2/examples/transient_file.py
-#
-# Run this code, then run transient_file.sh in another shell.
 import pyinotify
 import os
 
 class ProcessTransientFile(pyinotify.ProcessEvent):
 
-    def process_IN_MODIFY(self, event):
-        # We have explicitely registered for this kind of event.
-        print event.pathname, 'has modified.'
-        os.system("echo 'Hello Pyinotify!'")
+  def process_default(self, event):
+    # Implicitely IN_CREATE and IN_DELETE are watched too. You can
+    # ignore them and provide an empty process_default or you can
+    # process them, either with process_default or their dedicated
+    # method (process_IN_CREATE, process_IN_DELETE) which would
+    # override process_default.
+    print '(Pyinotify) ', event.maskname
 
-    def process_default(self, event):
-        # Implicitely IN_CREATE and IN_DELETE are watched too. You can
-        # ignore them and provide an empty process_default or you can
-        # process them, either with process_default or their dedicated
-        # method (process_IN_CREATE, process_IN_DELETE) which would
-        # override process_default.
-        print '(Pyinotify) ', event.maskname
+  def process_IN_MODIFY(self, event):
+    # We have explicitely registered for this kind of event.
+    print event.pathname, 'has modified.'
+    os.system("echo 'Hello Pyinotify!'")
 
+def main():
+  wm = pyinotify.WatchManager()
+  notifier = pyinotify.Notifier(wm)
+  # In this case you must give the class object (ProcessTransientFile)
+  # as last parameter not a class instance.
+  wm.watch_transient_file('/tmp/foo', pyinotify.IN_MODIFY, ProcessTransientFile)
+  notifier.loop()
 
-wm = pyinotify.WatchManager()
-notifier = pyinotify.Notifier(wm)
-# In this case you must give the class object (ProcessTransientFile)
-# as last parameter not a class instance.
-wm.watch_transient_file('/tmp/foo', pyinotify.IN_MODIFY, ProcessTransientFile)
-notifier.loop()
+if __name__ == "__main__":
+  main()
+
