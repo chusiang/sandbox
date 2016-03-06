190da448828e91133e78017690931d3ffb731fc5
chusiang <chusiang.lai@gmail.com>
2016-03-06 19:30:21 +0800

    run ansible with jupyter
    從原先在 host os 上使用 ansible 之架構更改成使用 guest os (jupyter, ipython notebook) 裡的 ansible，以便於撰寫筆記、同步實作環境 ... 等。

---
+++ ansible/init-jupyter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ============================================================
+#  Author: chusiang / chusiang.lai (at) gmail.com
+#  Blog: http://note.drx.tw
+#  Filename: install-jupyter.sh
+#  Modified: 2016-03-06 19:06
+#  Description: 
+#  Reference: 
+#
+#   1. Jupyter (aka IPython Notebook) Vagrantfile
+#    - https://gist.github.com/davemkirk/90140b1edde8d18c8b83
+#
+# =========================================================== 
+
+# Jupyter
+#
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip
+sudo pip install --upgrade ipython[all]
+
