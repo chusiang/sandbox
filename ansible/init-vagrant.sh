543d5a9b6553cf6dc8a92750c10afd9e0545819c
chusiang <chusiang.lai@gmail.com>
2016-03-14 09:54:52 +0800

    fix typo

--- ansible/init-vagrant.sh
+++ ansible/init-vagrant.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # switch apt mirror.
+## Ubuntu.
 sudo su -c "sed -i 's/archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list"
+## Debian.
+sudo su -c "sed -i 's/httpredir.debian.org/opensource.nchc.org.tw/g' /etc/apt/sources.list"
 sudo apt-get update
 
 # install other package.
 sudo apt-get install -y \
-  ack-grep
-  aptitude
+  ack-grep \
+  aptitude \
   git \
   less \
   make \
   mosh \
   tmux \
-  w3m \
+  w3m
 
